a9a0b9d1aa885c52714cbd5c0871cf23d62489d4
Marco Sandoval <segundo.marco@gmail.com>
2024-03-06 13:58:48 -0600

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/**/*',
+    // './app/helpers/**/*.rb',
+    // './app/javascript/**/*.js',
+    // './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ]
 }
